f5ef1c67c4fcfee770853edcb51ecc3865e3c01f
Boru <boru0713@gmail.com>
2024-06-29 11:24:26 +0800

    feat: add Bed Time and Wake Up Time
    bar plot and metrics

--- pages/1_Sleep_Analysis.py
+++ pages/1_Sleep_Analysis.py
@@ -1,6 +1,4 @@
-from datetime import timedelta
-
-import matplotlib.pyplot as plt
+from datetime import timedelta, date, time, datetime
 import numpy as np
 import pandas as pd
 import streamlit as st
@@ -9,11 +7,10 @@ import altair as alt
 
 @st.cache_data
 def get_df():
-    return pd.read_feather(st.session_state.data_path)
+    return pd.read_feather(st.session_state.data_path).drop_duplicates()
 
 def timedelta_to_hourminute(dt):
-    return f'{dt // 3600:.0f}h {(dt//60) % 60:.0f}m'
-
+    return f"{dt // 3600:.0f}h {(dt//60) % 60:.0f}m"
 
 st.set_page_config(
     page_title="Sleep Analysis",
@@ -21,8 +18,7 @@ st.set_page_config(
 )
 
 st.markdown("# Sleep Analysis")
-st.sidebar.header("Setting")
-st.write("""Choose start date and end date to analyze your daily in bed time.""")
+st.write("""Choose start date and end date to analyze your daily sleep time.""")
 
 # get data and limited its range
 df = get_df()
@@ -39,7 +35,6 @@ df = df.loc[df["duration"] >= timedelta(minutes=5)]  # remove durations <= 5 min
 df["idx"] = df["startDate"].apply(
     lambda x: x.date() if x.hour >= 18 else x.date() - timedelta(days=1)
 )
-
 start_date = st.date_input(
     "Start date",
     value=df["idx"].min(),
@@ -57,25 +52,22 @@ df = df.loc[df["idx"].between(start_date, end_date)]
 area_df = df.groupby("idx").duration.sum().reset_index()
 area_df["duration"] = area_df["duration"].dt.total_seconds()
 
+area_base = alt.Chart(area_df)
 
-c = (
-    (
-        alt.Chart(area_df)
-        .mark_area()
-        .encode(
-            x=alt.X("idx").title("date"),
-            y=alt.Y("duration").axis(
-                labelExpr='floor(datum.value/3600)+"h "+floor((datum.value%3600)/60)+"m"'
-            ),
-        )
+area_line = (
+    area_base.mark_area()
+    .encode(
+        x=alt.X("idx").title("date"),
+        y=alt.Y("duration").axis(
+            labelExpr='floor(datum.value/3600)+"h "+floor((datum.value%3600)/60)+"m"'
+        ),
     )
-    .properties(title=alt.Title("In Bed Time", fontSize=24))
     .interactive()
 )
 
 st.markdown("###")
-
-st.altair_chart(c, use_container_width=True)
+st.subheader("In Bed Time")
+st.altair_chart(area_line, use_container_width=True)
 
 # Add metrics
 col1, col2, col3 = st.columns(3)
@@ -83,6 +75,66 @@ q1 = np.quantile(area_df["duration"], 0.25)
 q2 = np.quantile(area_df["duration"], 0.5)
 q3 = np.quantile(area_df["duration"], 0.75)
 
-col1.metric(label="Median", value=timedelta_to_hourminute(q2), help='50% percentile') 
-col2.metric(label="Q1", value=timedelta_to_hourminute(q1), help='25% percentile')
-col3.metric(label="Q3", value=timedelta_to_hourminute(q3), help='75% percentile')
+col1.metric(label="Median", value=timedelta_to_hourminute(q2), help="50% percentile")
+col2.metric(label="Q1", value=timedelta_to_hourminute(q1), help="25% percentile")
+col3.metric(label="Q3", value=timedelta_to_hourminute(q3), help="75% percentile")
+
+st.markdown("###")
+st.subheader("Bed Time and Wake Up Time")
+
+line_df = (
+    df.groupby("idx").agg(
+        bed_time=("startDate", "min"),
+        wakeup_time=("endDate", "max"),
+    )
+).reset_index()
+
+line_df["idx"] = line_df["idx"].apply(lambda x: datetime.combine(x, time(hour=18)))
+line_df["bed_time"] -= line_df["idx"]
+line_df["wakeup_time"] -= line_df["idx"]
+# add date to simplify formatting
+line_df["bed_time"] = datetime(2024, 1, 1, 18, 0, 0) + line_df["bed_time"]
+line_df["wakeup_time"] = datetime(2024, 1, 1, 18, 0, 0) + line_df["wakeup_time"]
+line_df.rename(
+    columns={"bed_time": "Bed Time", "wakeup_time": "Wake Up Time"}, inplace=True
+)
+
+
+hist_base = (
+    alt.Chart(line_df)
+    .transform_fold(["Bed Time", "Wake Up Time"], as_=["Type", "Time"])
+    .transform_bin(field="Time", as_="Time", bin=alt.Bin(maxbins=100))
+    .encode(
+        color=alt.Color(
+            "Type:N",
+            scale=alt.Scale(
+                domain=["Bed Time", "Wake Up Time"], range=["#ddccbb", "red"]
+            ),
+        ),
+    )
+)
+
+hist_line = hist_base.mark_bar(opacity=0.4, binSpacing=0).encode(
+    alt.X("Time:T").axis(format="%H:%M"),
+    alt.Y("count()", stack=None),
+)
+
+hist_rule = hist_base.mark_rule(size=2, color="black").encode(
+    alt.X("median(Time):T").axis(format="%H:%M"),
+)
+
+st.altair_chart((hist_line + hist_rule).interactive(), use_container_width=True)
+
+# Add metrics
+col1, col2 = st.columns(2)
+bed_q2 = line_df["Bed Time"].median()
+wake_q2 = line_df["Wake Up Time"].median()
+
+col1.metric(
+    label="Median of Bed Time", value=bed_q2.strftime("%H:%M"), help="50% percentile"
+)
+col2.metric(
+    label="Median of Wake Up Time",
+    value=wake_q2.strftime("%H:%M"),
+    help="50% percentile",
+)
